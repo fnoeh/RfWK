d7781d790c871513a0e03b8ea04610b50aca82d8
Florian Nöhring <florian.noehring@gmail.com>
2019-08-27 17:10:15 +0200

    Initial Commit

---
+++ RfWK/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RfWK
+//
+//  Created by Florian Nöhring on 27.08.19.
+//  Copyright © 2019 Florian Nöhring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
